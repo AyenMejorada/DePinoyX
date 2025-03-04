acab4458958a5b5295a2d603ef50d7d80e6973ea
AyenMejorada <mejoradaayen1102@gmail.com>
2025-03-05 00:30:10 +0800

    Updated styles and fixed issues

--- de_pinoy_x/components/ui/button.tsx
+++ de_pinoy_x/components/ui/button.tsx
@@ -22,24 +22,26 @@ export default function Button({
 
         // "Play Now" variant
         variant === "play" &&
-          "bg-[#E3E3E3] text-black hover:bg-[#FF9311] hover:text-white",
+          `bg-[#E3E3E3] text-black 
+          ${disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-[#FF9311] hover:text-white"}`,
 
         // Bet variant
         variant === "bet" &&
           `border-2 border-[#FF9311] text-[#FF9311] bg-transparent 
-          hover:bg-[#FF9311] hover:text-[#121212]
-          ${selected ? "bg-[#FF9311] text-[#121212]" : ""}`,
+          ${disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-[#FF9311] hover:text-[#121212]"}
+          ${selected ? "bg-[#FF9311] text-[#E3E3E3]" : ""}`,
 
         // Color variant
         variant === "color" &&
           `w-[100px] h-[100px] ${colorClass} text-black 
-          transition-all transform hover:brightness-90
-          ${disabled ? "opacity-50 saturate-50" : ""}
+          transition-all transform
+          ${disabled ? "opacity-50 cursor-not-allowed" : "hover:brightness-90"}
           ${selected ? "shadow-[inset_0_0_20px_rgba(0,0,0,0.8)] scale-95" : ""}`,
 
         // Action variant
         variant === "action" &&
-          "bg-green-500 text-white hover:bg-green-600",
+          `bg-green-500 text-white 
+          ${disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-green-600"}`,
 
         className
       )}
